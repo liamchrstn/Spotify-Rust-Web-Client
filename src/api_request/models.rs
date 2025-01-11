12c23c8ef03fa8ac3e07216c29beacffe3a17f30
liam <liamchristian2004@gmail.com>
2025-01-11 12:32:01 +0000

    moved api things to rust

--- src/api_request/models.rs
+++ src/api_request/models.rs
@@ -10,6 +10,7 @@ pub struct Track {
     pub name: String,
     pub artists: Vec<Artist>,
     pub album: Album,
+    pub duration_ms: i32,
 }
 
 #[derive(Deserialize)]
@@ -45,4 +46,28 @@ pub struct StoredTracks {
     pub tracks: Vec<(String, String, String)>, // (track name, artist name, image url)
     pub total: i32,
     pub timestamp: u64,
+}
+
+#[derive(Deserialize)]
+pub struct CurrentPlaybackResponse {
+    pub item: Option<Track>,
+    pub is_playing: bool,
+    pub progress_ms: i32,
+}
+
+#[derive(Deserialize)]
+pub struct PlayerStateResponse {
+    pub shuffle_state: bool,
+}
+
+#[derive(Deserialize)]
+pub struct Device {
+    pub id: String,
+    pub name: String,
+    pub is_active: bool,
+}
+
+#[derive(Deserialize)]
+pub struct DevicesResponse {
+    pub devices: Vec<Device>,
 }
